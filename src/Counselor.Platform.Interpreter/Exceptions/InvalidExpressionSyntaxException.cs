539adb79ae01d2815866efca2b0ad37e77695a09
AxQE <axqe@tuta.io>
2021-04-12 11:08:12 +0700

    remove angular

---
+++ src/Counselor.Platform.Interpreter/Exceptions/InvalidExpressionSyntaxException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Counselor.Platform.Interpreter.Exceptions
+{
+	class InvalidExpressionSyntaxException : Exception
+	{
+		public InvalidExpressionSyntaxException()
+		{
+		}
+
+		public InvalidExpressionSyntaxException(string message) : base(message)
+		{
+		}
+
+		public InvalidExpressionSyntaxException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+	}
+}
