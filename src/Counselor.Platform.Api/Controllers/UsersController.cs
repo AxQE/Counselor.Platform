d224c9b679c93cba93ea6d7be8af2348e7a85671
AxQE <axqe@tuta.io>
2021-05-10 09:06:44 +0700

    api controllers

---
+++ src/Counselor.Platform.Api/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Counselor.Platform.Api.Entities.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Counselor.Platform.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersController : ControllerBase
+	{
+		[HttpPost]
+		public async Task<ActionResult<UserDto>> CreateUser(UserDto user)
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpGet("{id}")]		
+		public async Task<ActionResult<UserDto>> GetUser(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- src/Counselor.Platform.Api/Startup.cs
+++ src/Counselor.Platform.Api/Startup.cs
@@ -1,5 +1,6 @@
 using Counselor.Platform.Data.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,7 @@ using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Counselor.Platform.Api
@@ -29,7 +31,12 @@ namespace Counselor.Platform.Api
 		{
 			DatabaseDI.ConfigureDatabase(services, Configuration);
 
-			services.AddControllers();
+			services.AddControllers(c =>
+			{
+				c.RespectBrowserAcceptHeader = true;
+				c.ReturnHttpNotAcceptable = true;
+			});
+
 			services.AddSwaggerGen(c =>
 			{
 				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Counselor.Platform.Api", Version = "v1" });
@@ -37,7 +44,7 @@ namespace Counselor.Platform.Api
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
 		{
 			if (env.IsDevelopment())
 			{
@@ -55,6 +62,28 @@ namespace Counselor.Platform.Api
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapControllers();
+			});
+
+			ConfigureExceptionHandler(app, logger);
+		}
+
+		private static void ConfigureExceptionHandler(IApplicationBuilder app, ILogger<Startup> logger)
+		{
+			app.UseExceptionHandler(error =>
+			{
+				error.Run(async context =>
+				{
+					context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+					context.Response.ContentType = "application/json";
+
+					var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+					if (contextFeature != null)
+					{
+						logger.LogError($"Unhadled error during request: {contextFeature.Error}");
+						//await context.Response.WriteAsync(); todo: error response
+					}
+				});
 			});
 		}
 	}
