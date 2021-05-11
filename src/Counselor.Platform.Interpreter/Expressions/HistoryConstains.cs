390a6c5f581158a5d3ab4db17fd12b01833790dd
AxQE <axqe@tuta.io>
2021-05-11 13:15:09 +0700

    client moved to parent directory

---
+++ src/Counselor.Platform.Interpreter/Expressions/HistoryConstains.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counselor.Platform.Interpreter.Expressions
+{
+	class HistoryConstains : IExpression
+	{
+		public async Task Interpret(IContext context)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
